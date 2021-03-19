3aa1d867014e7e88ba7315ce70f91d6e53a6b6d5
MahimaMH <80575598+MahimaMH@users.noreply.github.com>
2021-03-19 19:55:58 +0530

    Update StringAssignment4.java

--- StringAssignment4.java
+++ StringAssignment4.java
@@ -6,24 +6,25 @@
 import java.util.Scanner;
 import java.lang.Math;
 public class StringAssignment4 {
-
+	static String temp;
 	public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 		
 		System.out.println("Enter the number");
 		int number=sc.nextInt();
-		System.out.println("the modified number is "+modifyNumber(number));
+		System.out.println("the modified number is "+modifyNumber(n) +temp);
 	}
-    private static int modifyNumber(int pNumber)
+    private static int modifyNumber(int n)
     {
-    	String str=Integer.toString(pNumber);
+    	String str=Integer.toString(n);
     	int output=0;
+	temp = Integer.toString(n%10);    
+	    
     	for(int i=0;i<str.length()-1;i++)
     	{
     		int diff=str.charAt(i)-str.charAt(i+1);
     		output=output*10+Math.abs(diff);
-    	}
-    	output=output*10+(str.charAt(str.length()-1))-48;
+	}
     	return output;
     }
 }
